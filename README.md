11191c230d837392076bfe1e774e5c6fc1f1c937
Glitch (power-rift-monday) <none>
2020-07-05 13:44:27 +0000

    📟⚡️ Updated with Glitch

--- README.md
+++
@@ -1,2 +0,0 @@
-# file-metadata-microservice
-Node.js microservice which returns file metadata
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const multer = require("multer");
+
 const app = express();
+
+app.use(express.static("public"));
 const upload = multer({ dest: "upload/" });
-app.use(express.static("./public"));
 app.post("/upload", upload.single("upfile"), (req, res) => {
-  console.log("hi");
   res.json(req.file);
 });
 
-app.listen(3000, () => console.log("running"));
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
